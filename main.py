39dd2d8effa15b353466af79f98a05f28e53c5c2
2Pillows <25855654+2Pillows@users.noreply.github.com>
2024-08-31 17:41:22 -0400

    fixed odds of getting/avoiding aspect

--- main.py
+++ main.py
@@ -66,9 +66,18 @@ class Recomb_Edge:
         total_suffixes = self.desired_suffix_count + self.exclusive_suffixes
 
         # if aspect suffix, need to get chances of avoiding / annulling
-        avoid_aspect_prob = 1 - self.aspect_suffix_count / max(
-            self.exclusive_suffixes, 1
-        )
+        chance_aspect = self.aspect_suffix_count / max(self.exclusive_suffixes, 1)
+
+        if (
+            self.final_prefix_count == 1
+            and self.final_suffix_count == 1
+            and self.desired_prefix_count == 1
+            and self.crafted_prefix_count == 2
+            and self.desired_suffix_count == 1
+            and self.crafted_suffix_count == 0
+            and self.aspect_suffix_count == 2
+        ):
+            print("check")
 
         # if prefix first, suffixes assume no exclusive, but requires there to be exclusive prefixes
         prefix_first = self._item_probability(
@@ -82,7 +91,7 @@ class Recomb_Edge:
             + (
                 1 if self.exclusive_prefixes == 0 else 0
             ),  # require 1 exclusive suffix if no exclusive prefix
-            avoid_aspect_prob,  # chance of avoiding aspect
+            chance_aspect,  # chance of getting aspect
         )
 
         suffix_first = self._item_probability(
@@ -96,19 +105,8 @@ class Recomb_Edge:
             + (
                 1 if self.exclusive_suffixes > 0 else 0
             ),  # only require at most 1 exclusive suffix
-            avoid_aspect_prob,  # chance of avoiding aspect
+            chance_aspect,  # chance of getting aspect
         )
-
-        # if (
-        #     self.final_prefix_count == 0
-        #     and self.final_suffix_count == 2
-        #     and self.desired_prefix_count == 0
-        #     and self.crafted_prefix_count == 0
-        #     and self.desired_suffix_count == 2
-        #     and self.crafted_suffix_count == 2
-        #     and self.aspect_suffix_count == 0
-        # ):
-        #     print("check")
 
         return 0.5 * (prefix_first + suffix_first)
 
@@ -121,7 +119,7 @@ class Recomb_Edge:
         # suffixes
         initial_suffixes,
         final_suffixes,
-        avoid_aspect_odds,
+        chance_aspect,
     ):
 
         if (
@@ -137,7 +135,7 @@ class Recomb_Edge:
 
         # if no suffixes are required, can lock prefix and wipe
         # aspect doesn't matter
-        if avoid_aspect_odds == 0 or final_suffixes == 0:
+        if chance_aspect == 0 or final_suffixes == 0:
             return prefix_prob * suffix_prob
 
         # need to get odds of avoiding or annuling aspect
@@ -149,14 +147,14 @@ class Recomb_Edge:
         # else
         # annuling is 1 / (final prefixes + final suffixes)
 
-        avoid_prob = suffix_prob * avoid_aspect_odds
+        avoid_prob = suffix_prob * (1 - chance_aspect)
 
         annul_odds = (
             1 / final_suffixes
             if self.eldritch_annul
             else 1 / (final_prefixes + final_suffixes)
         )
-        annul_prob = suffix_prob * (1 - avoid_aspect_odds) * annul_odds
+        annul_prob = suffix_prob * chance_aspect * annul_odds
 
         return prefix_prob * (avoid_prob + annul_prob)
 
